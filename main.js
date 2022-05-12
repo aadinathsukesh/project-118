846cc3c1c3fb5c9360d1cdf4cc2a1e3887ea5864
aadinathsukesh <85382361+aadinathsukesh@users.noreply.github.com>
2022-05-12 18:53:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+answer_holder="set";
+score="";
+
+
+
+
+function setup(){
+    canvas=createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function draw(){
+    check_sketch="";
+    if (drawn_sketch==sketch) {
+        
+    }
+}
+
+function clearCanvas(){
+    background("white");
+}
